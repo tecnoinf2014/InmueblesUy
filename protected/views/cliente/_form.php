7c40eaff4c285cffd306507fe496aab5de4f3949
jmg216 <juanma.miraballes@gmail.com>
2014-07-01 14:59:16 -0300

    CRUD - USUARIO - CLIENTE

---
+++ protected/views/cliente/_form.php
@@ -0,0 +1,58 @@
+<?php
+/* @var $this ClienteController */
+/* @var $model Cliente */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form=$this->beginWidget('CActiveForm', array(
+	'id'=>'cliente-form',
+	// Please note: When you enable ajax validation, make sure the corresponding
+	// controller action is handling ajax validation correctly.
+	// There is a call to performAjaxValidation() commented in generated controller code.
+	// See class documentation of CActiveForm for details on this.
+	'enableAjaxValidation'=>false,
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'nombre'); ?>
+		<?php echo $form->textField($model,'nombre',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'nombre'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'apellido'); ?>
+		<?php echo $form->textField($model,'apellido',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'apellido'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'email'); ?>
+		<?php echo $form->textField($model,'email',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'email'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'ci'); ?>
+		<?php echo $form->textField($model,'ci',array('size'=>10,'maxlength'=>10)); ?>
+		<?php echo $form->error($model,'ci'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'telefono'); ?>
+		<?php echo $form->textField($model,'telefono',array('size'=>45,'maxlength'=>45)); ?>
+		<?php echo $form->error($model,'telefono'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
